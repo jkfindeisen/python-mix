5c22957b24e054948154c8ee6eba3da31f82562e
jkfindeisen <jkfindeisen@users.noreply.github.com>
2021-04-14 14:45:49 +0200

    preparation

---
+++ obf/obf_support.py
@@ -0,0 +1,20 @@
+"""
+  Pure Python read only support for OBF files. Mimicks the File and Stack API of specpy (https://pypi.org/project/specpy/)
+  See https://github.com/AbberiorInstruments/ImspectorDocs/blob/master/docs/fileformat.rst for the format specification.
+
+
+
+  Author: Jan Keller-Findeisen
+"""
+
+
+class File():
+
+    def __init__(self):
+        pass
+
+
+class Stack():
+
+    def __init__(self):
+        pass
